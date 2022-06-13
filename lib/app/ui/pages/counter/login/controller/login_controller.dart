d8a99bdc4cdd9903a996bea06c1f2f81d870561e
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-06-12 23:23:39 -0400

    haciendo el stateNotifier que es para gestionar estados mas complejos

---
+++ lib/app/ui/pages/counter/login/controller/login_controller.dart
@@ -0,0 +1,6 @@
+import 'package:example_meedu/app/ui/pages/counter/login/controller/login_state.dart';
+import 'package:flutter_meedu/meedu.dart';
+
+class LoginController extends StateNotifier<LoginState> {
+  LoginController() : super(LoginState.initialState);
+}
